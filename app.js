772e3d5d70f11c3ad25ea9066c7f0ac6deafa731
liamquinn14 <138837924+liamquinn14@users.noreply.github.com>
2023-09-19 16:44:50 +0100

    fix concatenation after pressing equals
    now, after an answer has been given, when a number is pressed, the calculator wipes, starting a new calculation. Before, if the answer was 10, and then the user pressed '3' to start a new calculation, it would automatically start a new calculation with 103. This is fixed

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ let display = document.getElementById("display")
 let calcString = ""
 let calcArray
 let calc
+let answered = false
+
+let makeTrue = () => {
+    answered = true
+}
+
+let makeFalse = () => {
+    answered = false
+}
 
 let concat = (buttonNum) => {
-if (calcString.includes("-") || calcString.includes("+") || calcString.includes("/") || calcString.includes("*")) {
+    if (answered === true && buttonNum === "-" || answered === true && buttonNum === "+" || answered === true && buttonNum === "/" || answered === true && buttonNum === "*") {
+        calcString = calcString + buttonNum
+        display.innerText = calcString
+    } else if (answered === true && typeof Number(buttonNum) === "number") {
+        display.innerText = buttonNum
+        calcString = display.innerText
+    } else if (calcString.includes("-") || calcString.includes("+") || calcString.includes("/") || calcString.includes("*")) {
     if (buttonNum === "-" || buttonNum === "+" || buttonNum === "/" || buttonNum === "*") {
         display.innerText = findCalc(calcString) + buttonNum
         calcString = display.innerText
